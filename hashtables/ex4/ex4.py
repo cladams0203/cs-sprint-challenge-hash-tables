62278a35490639238438757a20157329d20af770
cladams0203 <cladams0203@gmail.com>
2020-09-11 21:39:57 -0400

    finished fourth problem

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # create hashtable
+    hashtable = {}
+    # create result array
+    result = []
+    # loop through and add all negative numbers to the hashtable converting to positive
+    for i in a:
+        if i < 0:
+            hashtable[abs(i)] = i
+    # search hashtable for matching key that was converted to positive
+    for i in a:
+        if i in hashtable:
+            result.append(i)
 
     return result
 
